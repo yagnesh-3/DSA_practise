d24310ba58baa516c0fd363da32fe73229dbc832
yagnesh-3 <162590670+yagnesh-3@users.noreply.github.com>
2024-10-16 10:59:48 +0530

    Time: 2 ms (46.69%) | Memory: 7.3 MB (98.54%) - LeetSync

--- 1304-longest-happy-string/longest-happy-string.cpp
+++ 1304-longest-happy-string/longest-happy-string.cpp
@@ -1,62 +1,63 @@
 class Solution {
 public:
     string longestDiverseString(int a, int b, int c) {
-        string s = "";
-        
-        while (a > 0 || b > 0 || c > 0) {
-            int n = s.size();
-            
-            // Check if the last two characters are the same
-            if (n >= 2 && s[n - 1] == s[n - 2]) {
-                if (s[n - 1] == 'a') {
-                    // 'a' was used twice, now pick 'b' or 'c'
-                    if (b >= c && b > 0) {
-                        s += 'b';
+        string s  = "";
+        while(a>0||b>0||c>0){
+            int n = s.length();
+            if(n>=2 && s[n-1] == s[n-2]){
+                
+                if(s[n-1] == 'a'){
+                    if(b>=c && b>0){
+                        s+="b";
                         b--;
-                    } else if (c > 0) {
-                        s += 'c';
+                    }else if(c>0){
+                        s+="c";
                         c--;
-                    } else {
-                        break; // No valid character left to use
-                    }
-                } else if (s[n - 1] == 'b') {
-                    // 'b' was used twice, now pick 'a' or 'c'
-                    if (a >= c && a > 0) {
-                        s += 'a';
-                        a--;
-                    } else if (c > 0) {
-                        s += 'c';
-                        c--;
-                    } else {
-                        break;
-                    }
-                } else if (s[n - 1] == 'c') {
-                    // 'c' was used twice, now pick 'a' or 'b'
-                    if (a >= b && a > 0) {
-                        s += 'a';
-                        a--;
-                    } else if (b > 0) {
-                        s += 'b';
-                        b--;
-                    } else {
+                    }else{
                         break;
                     }
                 }
-            } else {
-                // No two consecutive characters are the same, pick the largest available character
-                if (a >= b && a >= c && a > 0) {
-                    s += 'a';
+
+                
+                else if(s[n-1] == 'b'){
+                    if(a>=c && a>0){
+                        s+="a";
+                        a--;
+                    }else if(c>0){
+                        s+="c";
+                        c--;
+                    }else{
+                        break;
+                    }
+                }
+
+                
+               else if(s[n-1] == 'c'){
+                    if(a>=b && a>0){
+                        s+="a";
+                        a--;
+                    }else if(b>0){
+                        s+="b";
+                        b--;
+                    }else{
+                        break;
+                    }
+                }
+
+
+            }else{
+                if(a>=b && a>=c && a>0){
+                    s+="a";
                     a--;
-                } else if (b >= a && b >= c && b > 0) {
-                    s += 'b';
+                }else if(b>=c && b>0){
+                    s+="b";
                     b--;
-                } else if (c >= a && c >= b && c > 0) {
-                    s += 'c';
+                }else if(c>0){
+                    s+="c";
                     c--;
                 }
             }
         }
-        
         return s;
     }
 };
