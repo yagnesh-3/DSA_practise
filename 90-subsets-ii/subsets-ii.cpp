ce4f29596ebba4fa9da1111d4cd35316eb5cd8db
yagnesh-3 <162590670+yagnesh-3@users.noreply.github.com>
2024-10-18 14:44:47 +0530

    Time: 3 ms (55.89%) | Memory: 9.7 MB (39.15%) - LeetSync

--- 90-subsets-ii/subsets-ii.cpp
+++ 90-subsets-ii/subsets-ii.cpp
@@ -1,21 +1,21 @@
 class Solution {
 public:
-    set<vector<int>>ans;
-    void helper(int i,vector<int>&v,vector<int>&arr){
+    void helper(int i,vector<int>&v,vector<int>&arr,set<vector<int>>&ans){
         if(i==arr.size()){
             ans.insert(v);
             return;
         }
         v.push_back(arr[i]);
-        helper(i+1,v,arr);
+        helper(i+1,v,arr,ans);
         v.pop_back();
-        helper(i+1,v,arr);
+        helper(i+1,v,arr,ans);
 
     }
     vector<vector<int>> subsetsWithDup(vector<int>& nums) {
+        set<vector<int>>ans;
         vector<int>v;
         sort(nums.begin(),nums.end());
-        helper(0,v,nums);
+        helper(0,v,nums,ans);
         vector<vector<int>>fans;
         for(auto i : ans){
             fans.push_back(i);
