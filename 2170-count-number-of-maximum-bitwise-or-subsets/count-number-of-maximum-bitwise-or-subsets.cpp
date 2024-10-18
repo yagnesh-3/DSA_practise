53537beae5f68304c8101b277df716df92d153b3
yagnesh-3 <162590670+yagnesh-3@users.noreply.github.com>
2024-10-18 13:51:31 +0530

    Time: 2934 ms (5.12%) | Memory: 582.3 MB (5.10%) - LeetSync

--- 2170-count-number-of-maximum-bitwise-or-subsets/count-number-of-maximum-bitwise-or-subsets.cpp
+++ 2170-count-number-of-maximum-bitwise-or-subsets/count-number-of-maximum-bitwise-or-subsets.cpp
@@ -2,27 +2,22 @@
 class Solution {
 public:
     int ans = 0;
-    void helper(int i,vector<int>v,vector<int>arr,int target){
+    void helper(int i,vector<int>v,vector<int>arr,int target,int tor){
         if(i == arr.size()){
-            int tempOr = 0;
-            for(int to : v){
-                tempOr = tempOr|to;
-            }
-            if(tempOr == target){
+            if(tor == target)
             ans++;
-            }
             return;
         }
-        helper(i+1,v,arr,target);
+        helper(i+1,v,arr,target,tor);
         v.push_back(arr[i]);
-        helper(i+1,v,arr,target);
+        helper(i+1,v,arr,target,tor|arr[i]);
     }
     int countMaxOrSubsets(vector<int>& nums) {
          ios::sync_with_stdio(0);         cin.tie(0);
         int arrOr = 0;
         for(int i:nums) arrOr = arrOr|i;
         vector<int>v;
-        helper(0,v,nums,arrOr);
+        helper(0,v,nums,arrOr,0);
         return ans;
     }
 };
