7c5867ed2148296cedee1f011fe09b544c293972
yagnesh-3 <162590670+yagnesh-3@users.noreply.github.com>
2024-10-27 17:07:49 +0530

    Time: 7 ms (66.05%) | Memory: 30.5 MB (21.28%) - LeetSync

--- 221-maximal-square/maximal-square.cpp
+++ 221-maximal-square/maximal-square.cpp
@@ -9,7 +9,7 @@ public:
         for(int j = 0;j<n;j++){
             if(matrix[i][j] == '1' && (i == 0 || j ==0)) dp[i][j] = 1;
             else if(matrix[i][j] == '1') dp[i][j] = min(dp[i-1][j],min(dp[i-1][j-1] , dp[i][j-1] ))+1;
-            if(matrix[i][j] == '0') dp[i][j] = 0;
+            // if(matrix[i][j] == '0') dp[i][j] = 0;
             side = max(side,dp[i][j]);
 
         }
