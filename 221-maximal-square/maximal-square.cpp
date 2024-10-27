3d90c7543dd7737dde6292cf10ea48dea210183c
yagnesh-3 <162590670+yagnesh-3@users.noreply.github.com>
2024-10-27 17:08:49 +0530

    Time: 8 ms (57.94%) | Memory: 30.6 MB (9.35%) - LeetSync

--- 221-maximal-square/maximal-square.cpp
+++ 221-maximal-square/maximal-square.cpp
@@ -7,9 +7,10 @@ public:
         vector<vector<int>>dp(m,vector<int>(n,0));
         for(int i = 0;i<m;i++){
         for(int j = 0;j<n;j++){
-            if(matrix[i][j] == '1' && (i == 0 || j ==0)) dp[i][j] = 1;
-            else if(matrix[i][j] == '1') dp[i][j] = min(dp[i-1][j],min(dp[i-1][j-1] , dp[i][j-1] ))+1;
-            // if(matrix[i][j] == '0') dp[i][j] = 0;
+            if(matrix[i][j] == '1' ){
+                if (i == 0 || j ==0) dp[i][j] = 1;
+            else dp[i][j] = min(dp[i-1][j],min(dp[i-1][j-1] , dp[i][j-1] ))+1;
+            }
             side = max(side,dp[i][j]);
 
         }
