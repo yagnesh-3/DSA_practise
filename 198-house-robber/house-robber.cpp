becead9cde941e793d8cf47d169516879354433e
yagnesh-3 <162590670+yagnesh-3@users.noreply.github.com>
2024-11-11 18:29:37 +0530

    Time: 0 ms (100.00%) | Memory: 9.4 MB (63.52%) - LeetSync

--- 198-house-robber/house-robber.cpp
+++ 198-house-robber/house-robber.cpp
@@ -14,6 +14,13 @@ public:
     int rob(vector<int>& nums) {
         int n = nums.size();
         vector<int>dp(n,-1);
-        return f(n-1,nums,dp);
+        int neg = 0;
+        dp[0] = nums[0];
+        for(int i= 1;i<n;i++){
+            int take =(i>1)?nums[i]+dp[i-2] :nums[i]+0;
+            int nottake = 0+dp[i-1];
+            dp[i] = max(take,nottake);
+        }
+        return dp[n-1];
     }
 };
