528cb188f7ea12ba2afa1cd8e37b4299e2de735c
yagnesh-3 <162590670+yagnesh-3@users.noreply.github.com>
2024-11-11 18:29:44 +0530

    Time: 0 ms (100.00%) | Memory: 9.4 MB (63.52%) - LeetSync

--- 198-house-robber/house-robber.cpp
+++ 198-house-robber/house-robber.cpp
@@ -14,7 +14,6 @@ public:
     int rob(vector<int>& nums) {
         int n = nums.size();
         vector<int>dp(n,-1);
-        int neg = 0;
         dp[0] = nums[0];
         for(int i= 1;i<n;i++){
             int take =(i>1)?nums[i]+dp[i-2] :nums[i]+0;
