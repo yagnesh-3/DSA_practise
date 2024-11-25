bfc2c0eec7a03e5548395bdffadf1a77859f9fe5
yagnesh-3 <162590670+yagnesh-3@users.noreply.github.com>
2024-11-25 17:47:57 +0530

    Time: 29 ms (43.49%) | Memory: 26 MB (30.28%) - LeetSync

--- 1250-longest-common-subsequence/longest-common-subsequence.cpp
+++ 1250-longest-common-subsequence/longest-common-subsequence.cpp
@@ -16,7 +16,7 @@ public:
                 if(text1[i-1] == text2[j-1]){
                     dp[i][j] = dp[i-1][j-1]+1;
                 }else{
-                    dp[i][j] = max(dp[i-1][j-1],max(dp[i-1][j],dp[i][j-1]));
+                    dp[i][j] = max(dp[i-1][j],dp[i][j-1]);
                 }
             }
         }
