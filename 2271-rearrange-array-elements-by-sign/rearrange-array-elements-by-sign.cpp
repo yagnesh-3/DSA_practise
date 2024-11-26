ffbb777ff6a70125b173d9e926a9f4c6af0d774e
yagnesh-3 <162590670+yagnesh-3@users.noreply.github.com>
2024-11-26 16:45:46 +0530

    Time: 0 ms (100.00%) | Memory: 128.4 MB (41.48%) - LeetSync

--- 2271-rearrange-array-elements-by-sign/rearrange-array-elements-by-sign.cpp
+++ 2271-rearrange-array-elements-by-sign/rearrange-array-elements-by-sign.cpp
@@ -2,7 +2,7 @@ class Solution {
 public:
     vector<int> rearrangeArray(vector<int>& nums) {
         int n = nums.size();
-        vector<int>ans(n,0);
+        vector<int>ans(n);
         int pind = 0;
         int nind = 1;
         for(int i = 0;i<n;i++){
